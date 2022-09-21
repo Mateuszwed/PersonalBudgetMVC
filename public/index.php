479625582819cd4b174d437c9a1242ce097fce73
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-09-21 11:44:55 +0200

    create remebered login function

--- public/index.php
+++ public/index.php
@@ -6,19 +6,21 @@
  * PHP version 7.0
  */
 
+ini_set('session.cookie_lifetime', '864000');
+
 /**
  * Composer
  */
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 
-/**
- * Error and Exception handling
- */
 error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
+
+
+session_start();
 
 /**
  * Routing
