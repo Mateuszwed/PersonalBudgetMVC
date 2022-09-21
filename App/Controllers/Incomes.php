30bd8f0023a5768e07e2a256f476db76264f41f7
Mateusz Szwed <mateusz.szwed.programista@gmail.com>
2022-09-21 11:48:54 +0200

    create add incomes contorller

---
+++ App/Controllers/Incomes.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controllers;
+
+use \App\Models\Income;
+use \App\Models\SettingsData;
+use \Core\View;
+use \App\Auth;
+use \App\Flash;
+
+/**
+ * Items controller (example)
+ *
+ * PHP version 7.0
+ */
+//class Items extends \Core\Controller
+class Incomes extends Authenticated
+{
+
+    public function addAction()
+    {
+        View::renderTemplate('Incomes/add.html');
+    }
+	
+	 public function createAction()
+    {
+        $user = Auth::getUser();
+        $income = new Income($_POST);
+        
+        if ($income->save($user->id)) {
+
+            View::renderTemplate('Incomes/success.html');
+
+        } else {
+            Flash::addMessage('Nie udało się zarejestrować przychodu.', Flash::WARNING);
+            
+            $settings = new SettingsData();
+            $categoriesIncomes = $settings->getIncomesCategories();
+        
+            View::renderTemplate('Incomes/add.html', [
+                'income' => $income,
+                'categoriesIncomes' => $categoriesIncomes
+            ]);
+        }
+    }
+
+}
